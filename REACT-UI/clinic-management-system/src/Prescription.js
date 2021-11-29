1b961e786a081c4349e7feb1eef7b8f3391ba879
ajay.mk@experionglobal.com <ajaypranav1999@gmail.com>
2021-11-29 14:51:22 +0530

    hi

--- REACT-UI/clinic-management-system/src/Prescription.js
+++ REACT-UI/clinic-management-system/src/Prescription.js
@@ -1,7 +1,5 @@
-//code for the prescription form here
-<<<<<<< HEAD
+//code for the prescription form her
 const a = 100;
-=======
+
 const a = 5;
 const b=10;
->>>>>>> 37ca450ea6281d63b03b83a39ca9bbca4ff47562
