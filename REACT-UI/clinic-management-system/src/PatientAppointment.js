2e68ae88144e5139f11e1460f94ef66a0870ab22
megha.pm <megha.pm@experionglobal.com>
2021-11-29 15:36:12 +0530

    change

--- REACT-UI/clinic-management-system/src/PatientAppointment.js
+++ REACT-UI/clinic-management-system/src/PatientAppointment.js
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 import axios from 'axios';
 
-function RegisterMedicine(){
+function PatientAppointment(){
     localStorage.clear();
     return (
         <>
@@ -90,4 +90,4 @@ function MyForm(props){
     );
 
 };
-export default RegisterMedicine;
+export default  PatientAppointment ;
