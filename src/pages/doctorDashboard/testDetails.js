34545fa45e09f58489dddda24e4d5fd0caba4421
Jijo <jijo8595@gmail.com>
2021-12-04 11:08:31 +0530

    style in testDetails

--- src/pages/doctorDashboard/testDetails.js
+++ src/pages/doctorDashboard/testDetails.js
@@ -17,7 +17,7 @@ function TestDetails(){
         })},[id])
         
 
-        return(<>
+       /* return(<>
         <h2>Test Details</h2>
         
         <div>
@@ -30,7 +30,24 @@ function TestDetails(){
       
         <a href="/appointmentlist">Go Back</a>
         </div>
-        </>)}
+       </>)*/
+
+       return(
+        <Card className="text-center">
+        <Card.Header>Test Details</Card.Header>
+        <Card.Body>
+          <Card.Title> Test Name : {tests.testName}</Card.Title>
+          <Card.Title> Discription: {tests.description}</Card.Title>
+          <Card.Title> Desired Range :{tests.desiredRange}</Card.Title>
+          <Card.Title> Result Value :{tests.resultValue}</Card.Title>
+          <Card.Title> Remark :{tests.remarks} </Card.Title>
+        </Card.Body>
+        <Card.Footer className="text-muted"><a href="/appointmentlist">Go Back</a></Card.Footer>
+      </Card>
+
+      )
+    
+    }
 
    
 export default TestDetails;
